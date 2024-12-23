198729d0c4414d20dacaa69540321cb5c19b2f31
Suraj MS <surajmshivakumar@gmail.com>
2024-12-23 19:47:29 +0530

    update skill

--- src/pages/SkillLogos.jsx
+++ src/pages/SkillLogos.jsx
@@ -1,37 +1,46 @@
 import React from 'react';
-import { FaReact, FaNodeJs, FaHtml5, FaCss3Alt, FaAws, FaPython, FaJava, FaAngular, FaLinux, FaGit, FaDocker  } from "react-icons/fa";
-import { SiExpress, SiMysql, SiAdobexd, SiTypescript, SiDjango,      } from "react-icons/si";
+import { FaReact, FaNodeJs, FaHtml5, FaCss3Alt, FaAws, FaPython, FaJava, FaAngular, FaLinux, FaGit, FaDocker } from "react-icons/fa";
+import { SiExpress, SiMysql, SiAdobexd, SiTypescript, SiDjango, SiGooglemaps } from "react-icons/si";
 import { DiMongodb } from "react-icons/di";
+import { IoLogoFirebase } from "react-icons/io5";
+import { RiGeminiFill } from "react-icons/ri";
+import { TbBrandReactNative } from "react-icons/tb";
 
 const skills = [
-  { id: 1, icon: <FaReact />, name: 'React' },
-  { id: 2, icon: <FaNodeJs />, name: 'Node.js' },
-  { id: 3, icon: <SiExpress />, name: 'Express jS' },
-  { id: 10, icon: < FaPython  />, name: 'Python' },
-  { id: 4, icon: <DiMongodb  />, name: 'MongoDb' },
-  { id: 5, icon: < SiMysql  />, name: 'My Sql' },
-  { id: 6, icon: < FaHtml5  />, name: 'HTML' },
-  { id: 7, icon: <FaCss3Alt />, name: 'CSS' },
-  { id: 8, icon: < FaAws  />, name: 'AWS' },
-  { id: 9, icon: < SiAdobexd  />, name: 'Adobe XD' },
-  { id: 10, icon: < FaJava   />, name: 'Java' },
-  { id: 10, icon: < FaAngular    />, name: 'Angular' },
-  { id: 10, icon: < SiTypescript     />, name: 'Type Script' },
-  { id: 10, icon: < SiDjango />, name: 'Django' },
-  { id: 10, icon: < FaDocker  />, name: 'Docker' },
-  { id: 10, icon: < FaLinux  />, name: 'Linux' },
-  { id: 10, icon: < FaGit/>, name: 'Git' },
+  { id: 1, icon: <FaReact />, name: 'React', url: 'https://reactjs.org/' },
+  { id: 2, icon: <FaNodeJs />, name: 'Node.js', url: 'https://nodejs.org/' },
+  { id: 3, icon: <SiExpress />, name: 'Express JS', url: 'https://expressjs.com/' },
+  { id: 4, icon: <DiMongodb />, name: 'MongoDB', url: 'https://www.mongodb.com/' },
+  { id: 5, icon: <SiMysql />, name: 'MySQL', url: 'https://www.mysql.com/' },
+  { id: 17, icon: <SiGooglemaps />, name: 'GoogleMaps', url: 'https://developers.google.com/maps' },
+  { id: 18, icon: <IoLogoFirebase />, name: 'Firebase', url: 'https://firebase.google.com/' },
+  { id: 19, icon: <RiGeminiFill />, name: 'Gemini', url: 'https://gemini.com/' },
+  { id: 20, icon: <TbBrandReactNative />, name: 'ReactNative', url: 'https://reactnative.dev/' },
+  { id: 6, icon: <FaHtml5 />, name: 'HTML', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+  { id: 7, icon: <FaCss3Alt />, name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+  { id: 8, icon: <FaAws />, name: 'AWS', url: 'https://aws.amazon.com/' },
+  { id: 9, icon: <SiAdobexd />, name: 'Adobe XD', url: 'https://adobexdplatform.com/' },
+  { id: 10, icon: <FaJava />, name: 'Java', url: 'https://www.java.com/' },
+  { id: 11, icon: <FaAngular />, name: 'Angular', url: 'https://angular.io/' },
+  { id: 12, icon: <SiTypescript />, name: 'TypeScript', url: 'https://www.typescriptlang.org/' },
+  { id: 13, icon: <SiDjango />, name: 'Django', url: 'https://www.djangoproject.com/' },
+  { id: 14, icon: <FaDocker />, name: 'Docker', url: 'https://www.docker.com/' },
+  { id: 15, icon: <FaLinux />, name: 'Linux', url: 'https://www.linux.org/' },
+  { id: 16, icon: <FaGit />, name: 'Git', url: 'https://git-scm.com/' },
+
 ];
 
 const SkillLogos = () => {
   return (
     <div>
-        <h1 style={{ fontSize: '3rem', textAlign: 'center' }}>Skills</h1>
+      <h1 style={{ fontSize: '3rem', textAlign: 'center' }}>Skills</h1>
       <ul className='skill_logos'>
         {skills.map(skill => (
-          <li className='skill_logos_list' key={skill.id} >
-          <p>{skill.icon}</p> <p>{skill.name}</p>
-        </li>
+          <li className='skill_logos_list' key={skill.id}>
+            <a href={skill.url} target="_blank" rel="noopener noreferrer">
+              <p>{skill.icon}</p> <p>{skill.name}</p>
+            </a>
+          </li>
         ))}
       </ul>
     </div>
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import '../styles/Skills.css'
 import { FaAws, FaFigma } from "react-icons/fa";
 import { DiMongodb } from "react-icons/di";
-import { SiMysql, SiExpress, SiAdobexd } from "react-icons/si";
+import { SiMysql, SiExpress, SiAdobexd, SiGooglemaps } from "react-icons/si";
 import web from '../assets/images/web.png'
 import cloud from '../assets/images/cloud.png'
+import brain from '../assets/images/3d brain.png'
 import graphics from '../assets/images/graphics.png'
+import { IoLogoFirebase } from "react-icons/io5";
+import { RiGeminiFill } from "react-icons/ri";
 import SkillLogos from './SkillLogos';
 
 
-
-
-
 const Skills = () => {
+    const handleNavigation = (url) => {
+        window.open(url, '_blank');
+    };
+
     return (
         <div>
             <h1 style={{ fontSize: '3rem', textAlign: 'center' }}>What I Do?</h1>
@@ -23,17 +27,17 @@ const Skills = () => {
                 <div className="skill_item skill_data">
                     <h1 className="skill_item_head">Full Stack Development</h1>
                     <ul className="skill_item_img">
-                        <li><p className='skill_item_img_title'>Node Js</p> <ion-icon name="logo-nodejs"></ion-icon></li>
-                        <li><p className='skill_item_img_title'>React</p> <ion-icon name="logo-react"></ion-icon></li>
-                        <li><p className='skill_item_img_title'>Express</p> <SiExpress className="skill_item_img_icon"/></li>
-                        <li><p className='skill_item_img_title'>Docker</p> <ion-icon name="logo-docker"></ion-icon></li>
-                        <li><p className='skill_item_img_title'>Python</p> <ion-icon name="logo-python"></ion-icon></li>
-                        <li><p className='skill_item_img_title'>Angular</p> <ion-icon name="logo-angular"></ion-icon></li>
+                        <li onClick={() => handleNavigation('https://nodejs.org/')}><p className='skill_item_img_title'>Node Js</p> <ion-icon name="logo-nodejs"></ion-icon></li>
+                        <li onClick={() => handleNavigation('https://reactjs.org/')}><p className='skill_item_img_title'>React</p> <ion-icon name="logo-react"></ion-icon></li>
+                        <li onClick={() => handleNavigation('https://expressjs.com/')}><p className='skill_item_img_title'>Express</p> <SiExpress className="skill_item_img_icon" /></li>
+                        <li onClick={() => handleNavigation('https://www.docker.com/')}><p className='skill_item_img_title'>Docker</p> <ion-icon name="logo-docker"></ion-icon></li>
+                        <li onClick={() => handleNavigation('https://www.python.org/')}><p className='skill_item_img_title'>Python</p> <ion-icon name="logo-python"></ion-icon></li>
+                        <li onClick={() => handleNavigation('https://angular.io/')}><p className='skill_item_img_title'>Angular</p> <ion-icon name="logo-angular"></ion-icon></li>
                     </ul>
                     <ul className="skill_item_body">
-                        <li><ion-icon name="flash-outline"></ion-icon> <p>Build, manage, and improve server-side operating systems, databases, libraries, and frameworks</p></li>
-                        <li><ion-icon name="flash-outline"></ion-icon> <p>Building resposive websites and deploy.</p></li>
-                        <li><ion-icon name="flash-outline"></ion-icon> <p>Creating application using MERN stack</p></li>
+                        <li><ion-icon name="flash-outline"></ion-icon> <p>End-to-End Development: Full-stack development enables creating both the client-side (frontend) and server-side (backend) of applications.</p></li>
+                        <li><ion-icon name="flash-outline"></ion-icon> <p>Manage the entire software development lifecycle, from planning and design to deployment and maintenance.</p></li>
+                        <li><ion-icon name="flash-outline"></ion-icon> <p>Strong problem-solving skills and the ability to integrate various technologies seamlessly.</p></li>
                     </ul>
                 </div>
 
@@ -41,34 +45,55 @@ const Skills = () => {
             <div className="skill">
 
                 <div className="skill_item skill_data i2d">
-                    <h1 className="skill_item_head">Cloud Infra-Architecture</h1>
+                    <h1 className="skill_item_head">Artificial Intelligence</h1>
                     <ul className="skill_item_img">
-                    <li><p className='skill_item_img_title'>AWS</p> <FaAws className="skill_item_img_icon" /></li>
-                    <li><p className='skill_item_img_title'>MongoDb</p> <DiMongodb className="skill_item_img_icon" /></li>
-                    <li><p className='skill_item_img_title'>Sql</p> <SiMysql className="skill_item_img_icon" /></li>
+                        <li onClick={() => handleNavigation('https://ai.google.dev/')}><p className='skill_item_img_title'>Gemini</p> <RiGeminiFill className="skill_item_img_icon" /></li>
                     </ul>
                     <ul className="skill_item_body">
-                        <li><ion-icon name="flash-outline"></ion-icon> <p>Experience working on multiple cloud platforms</p></li>
-                        <li><ion-icon name="flash-outline"></ion-icon> <p>Hosting and maintaining applications on virtual machine instances along with integration of databases</p></li>
+                        <li><ion-icon name="flash-outline"></ion-icon> <p>Utilize Gemini in your application via Google's AI platform to generate and analyze data with advanced language and reasoning capabilities.</p></li>
+                        <li><ion-icon name="flash-outline"></ion-icon> <p>Experience working on multiple AI platforms</p></li>
                     </ul>
                 </div>
 
                 <div className="skill_item i2i">
-                    <img src={cloud} alt="Programming Icon" width="80%" height="80%" />
+                    <img src={brain} alt="Programming Icon" width="80%" height="80%" />
                 </div>
 
             </div>
             <div className="skill">
 
-                <div className="skill_item">
-                    <img src={graphics} alt="Programming Icon" width="80%" height="80%" />
+                <div className="skill_item ">
+                    <img src={cloud} alt="Programming Icon" width="80%" height="80%" />
                 </div>
+                <div className="skill_item skill_data ">
+
+                    <h1 className="skill_item_head">Cloud Infra-Architecture</h1>
+                    <ul className="skill_item_img">
+                        <li onClick={() => handleNavigation('https://aws.amazon.com/')}><p className='skill_item_img_title'>AWS</p> <FaAws className="skill_item_img_icon" /></li>
+                        <li onClick={() => handleNavigation('https://www.mongodb.com/')}><p className='skill_item_img_title'>MongoDb</p> <DiMongodb className="skill_item_img_icon" /></li>
+                        <li onClick={() => handleNavigation('https://www.mysql.com/')}><p className='skill_item_img_title'>Sql</p> <SiMysql className="skill_item_img_icon" /></li>
+                        <li onClick={() => handleNavigation('https://firebase.google.com/')}><p className='skill_item_img_title'>Firbase</p> <IoLogoFirebase className="skill_item_img_icon" /></li>
+                        <li onClick={() => handleNavigation('https://mapsplatform.google.com/')}><p className='skill_item_img_title'>Google Maps</p> <SiGooglemaps className="skill_item_img_icon" /></li>
+                    </ul>
+                    <ul className="skill_item_body">
+                        <li><ion-icon name="flash-outline"></ion-icon> <p>Leverage Google Maps Platform to retrieve precise location data and integrate it into applications for features like navigation, geocoding, and location-based services.</p></li>
+                        <li><ion-icon name="flash-outline"></ion-icon> <p>Firebase simplifies backend development by providing real-time databases, authentication, hosting, and cloud functions for seamless app integration.</p></li>
+                        <li><ion-icon name="flash-outline"></ion-icon> <p>Experience working on multiple cloud platforms</p></li>
+                        <li><ion-icon name="flash-outline"></ion-icon> <p>Hosting and maintaining applications on virtual machine instances along with integration of databases</p></li>
+                    </ul>
+                </div>
+
+
+
+            </div>
+            <div className="skill">
+
 
                 <div className="skill_item skill_data">
                     <h1 className="skill_item_head">UI/UX Design</h1>
                     <ul className="skill_item_img">
-                    <li><p className='skill_item_img_title'>Adobe Xd</p><SiAdobexd className="skill_item_img_icon" /> </li>
-                    <li><p className='skill_item_img_title'>Figma</p> <FaFigma  className="skill_item_img_icon" /></li>
+                        <li onClick={() => handleNavigation('https://adobexdplatform.com/')}><p className='skill_item_img_title'>Adobe Xd</p><SiAdobexd className="skill_item_img_icon" /> </li>
+                        <li onClick={() => handleNavigation('https://www.figma.com/')}><p className='skill_item_img_title'>Figma</p> <FaFigma className="skill_item_img_icon" /></li>
 
 
                     </ul>
@@ -80,10 +105,14 @@ const Skills = () => {
                 </div>
 
 
+                <div className="skill_item">
+                    <img src={graphics} alt="Programming Icon" width="80%" height="80%" />
+                </div>
+
 
             </div>
 
-            <SkillLogos/>
+            <SkillLogos />
 
 
         </div>
