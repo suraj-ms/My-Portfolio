8fd1be1d5f8469100e9bcd93754021d22f78e404
Suraj MS <surajmshivakumar@gmail.com>
2025-01-06 13:10:53 +0530

    update resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,28 @@
 import SurajResume from '../assets/images/SurajResume.jpg'
-
+import SurajMSResume from '../assets/SurajMSResume.pdf'
 
 const PdfViewer = () => {
-
-
     return (
-        <div style={{display:'flex'}}>
-           <img src={SurajResume} width='80%' style={{margin: 'auto'}} alt="" />
+        <div style={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center'}}>
+            <a 
+                href={SurajMSResume} 
+                download="SurajResume.jpg"  // specify the file name for download
+                style={{
+                    padding: '10px 20px',
+                    border: '1px solid #fff',
+                    background:"none",
+                    color: 'white',
+                    borderRadius: '5px',
+                    cursor: 'pointer',
+                    marginBottom: '20px',
+                    textDecoration: 'none', // to remove underline from the link
+                    textAlign: 'center'
+                }}
+            >
+                Download Resume
+            </a>
+
+            <img src={SurajResume} width='80%' style={{margin: 'auto'}} alt="Suraj's Resume" />
         </div>
     );
 };
